a43168b6dc19f273baa6421b5d7061054bffe8bf
Subbotin Klim <subbotin_klim@pm.me>
2021-09-16 00:54:56 +0300

    добавлена игра "Проверка на чётность"

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/paparrot/php-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/paparrot/php-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/864806612d58bef6b1df/maintainability)](https://codeclimate.com/github/paparrot/php-project-lvl1/maintainability)
 ![PHP Codesniffer](https://github.com/paparrot/php-project-lvl1/actions/workflows/php.yml/badge.svg)
+
+[![Brain-Even](https://asciinema.org/a/436001.svg)](https://asciinema.org/a/436001)
---
+++ src/Even.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Brain\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+function start()
+{
+
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
+    line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+
+    $scores = 0;
+    while ($scores < 3) {
+        $randomInt = rand(1, 99);
+        $isEven = $randomInt % 2 == 0;
+        line("Question: {$randomInt}");
+        $answer = prompt("Your answer");
+        $rightAnswer = $isEven ? 'yes' : 'no';
+        $isCorrect = strtolower($answer) == $rightAnswer;
+
+        if (!$isCorrect) {
+            line("'{$answer}' is wrong answer ;(. Correct answer was {$rightAnswer}.");
+            line("Let's try again, {$name}!");
+            break;
+        } else {
+            line("Correct!");
+            $scores += 1;
+        }
+    }
+
+    if ($scores == 3) {
+        line("Congratulations, {$name}");
+    }
+}
